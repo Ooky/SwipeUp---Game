e337100867200b08b157a01a49dc89e151fffea7
micromic5 <mike.amacker@gmx.ch>
2016-11-20 19:44:58 +0100

    Revert "alt+shift+f"
    This reverts commit 4a2c1a5379a066aef9558a2ab3f9ec9254e110fd.

--- core/src/Tools/MyGestureListener.java
+++ core/src/Tools/MyGestureListener.java
@@ -5,78 +5,78 @@ import com.badlogic.gdx.math.Vector2;
 import screens.PlayScreen;
 
 public class MyGestureListener implements GestureDetector.GestureListener {
-
-	private PlayScreen screen;
-
-	public MyGestureListener(PlayScreen screen) {
+		private PlayScreen screen;
+	
+	public MyGestureListener(PlayScreen screen){
 		super();
 		this.screen = screen;
 	}
+	
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
 
-	@Override
-	public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
 
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {		
+        if(Math.abs(velocityX)>Math.abs(velocityY)){
+            if(velocityX>0){
+                    onRight();
+            }else{
+                    onLeft();
+            }
+        }else{
+            if(velocityY>0){
+                    onDown();
+            }else{                                  
+                    onUp();
+            }
+        }
 		return false;
-	}
 
-	@Override
-	public boolean tap(float x, float y, int count, int button) {
+    }
 
-		return false;
-	}
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
 
-	@Override
-	public boolean longPress(float x, float y) {
+        return false;
+    }
 
-		return false;
-	}
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
 
-	@Override
-	public boolean fling(float velocityX, float velocityY, int button) {
-		if (Math.abs(velocityX) > Math.abs(velocityY)) {
-			if (velocityX > 0) {
-				onRight();
-			} else {
-				onLeft();
-			}
-		} else {
-			if (velocityY > 0) {
-				onDown();
-			} else {
-				onUp();
-			}
-		}
-		return false;
+        return false;
+    }
 
-	}
+    @Override
+    public boolean zoom (float originalDistance, float currentDistance){
 
-	@Override
-	public boolean pan(float x, float y, float deltaX, float deltaY) {
+       return false;
+    }
 
-		return false;
-	}
-
-	@Override
-	public boolean panStop(float x, float y, int pointer, int button) {
-
-		return false;
-	}
-
-	@Override
-	public boolean zoom(float originalDistance, float currentDistance) {
-
-		return false;
-	}
-
-	@Override
-	public void pinchStop() {
-	}
+    @Override
+    public void pinchStop () {
+    }
 
 	@Override
 	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
 		return false;
 	}
-
+	
+	
 	//Swipe directions
 	public void onUp() {
 	}
